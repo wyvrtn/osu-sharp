5522d608f7afc625993aca039f1504c30783f28a
minisbett <39670899+minisbett@users.noreply.github.com>
2024-08-20 13:28:44 +0200

    add description attributes to ranked statuses

--- OsuSharp/Enums/RankedStatus.cs
+++ OsuSharp/Enums/RankedStatus.cs
@@ -13,36 +13,42 @@ public enum RankedStatus
   /// <summary>
   /// The beatmap is in the graveyard.
   /// </summary>
+  [Description("graveyard")]
   Graveyard = -2,
 
   /// <summary>
   /// The beatmap is a work in progress.
   /// </summary>
+  [Description("wip")]
   WIP = -1,
 
   /// <summary>
   /// The beatmap is pending a rank status evaluation.
   /// </summary>
-  [Description("Pending")]
+  [Description("pending")]
   Pending = 0,
 
   /// <summary>
   /// The beatmap is ranked.
   /// </summary>
+  [Description("ranked")]
   Ranked = 1,
 
   /// <summary>
   /// The beatmap is approved.
   /// </summary>
+  [Description("approved")]
   Approved = 2,
 
   /// <summary>
   /// The beatmap is qualified.
   /// </summary>
+  [Description("qualified")]
   Qualified = 3,
 
   /// <summary>
   /// The beatmap is loved.
   /// </summary>
+  [Description("loved")]
   Loved = 4
 }
